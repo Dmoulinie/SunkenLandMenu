0f43d301c9e1bc0cfb2a4c09ecbc12f35f692370
romai <romai@DAN-PC>
2023-09-02 19:29:54 +1100

    Ajoutez des fichiers projet.

---
+++ SunkenLandMenu/Hack.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+
+public class Hack : MonoBehaviour
+{
+    public void Start()
+    {
+        //Do stuff here once for initialization
+    }
+    public void Update()
+    {
+        //Do stuff here on every tick
+    }
+}
---
+++ SunkenLandMenu/Loader.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+namespace SunkenLandMenu
+{
+    class Loader
+    {
+        public static void Init()
+        {
+
+            Loader.Load = new GameObject();
+            Loader.Load.AddComponent<Hack>();
+            UnityEngine.Object.DontDestroyOnLoad(Loader.Load);
+        }
+
+        private static GameObject Load;
+    }
+}
